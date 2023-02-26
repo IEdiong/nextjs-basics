f999e7e5f24120d9ec2e701e556ef5ce3110c951
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-02-26 15:05:48 +0100

    Add finishing touch to index page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
 import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
 
 // Using SSG
 export async function getStaticProps() {
@@ -50,11 +51,11 @@ export default function Home({ allPostsData }) {
               className={utilStyles.listItem}
               key={id}
             >
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
